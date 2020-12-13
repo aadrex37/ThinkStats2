dac6774c990e17092ceb6a412ed2098a73f7548c
aadrex37 <70550383+aadrex37@users.noreply.github.com>
2020-12-13 09:59:47 -0500

    Create adrexlerDSC530.2.1.py

---
+++ adrexlerDSC530.2.1.py
@@ -0,0 +1,27 @@
+#Aaron Drexler
+#DSC530
+#12/12/20
+#Assignment 2.1
+#Python coding assignment
+
+print("Hello World! I wonder why that is a;ways the default coding text to start with")
+a = int(4)
+b = int(2)
+c = int(a+b)
+d = int(a-b)
+e = int(a*b)
+f = int(a/b)
+g = "Chicago "
+h = "Cubs"
+i = g + h
+j = "Chicago Bears"
+k = "Chicago Bulls"
+l = "Chicago Blackhawks"
+m = [i, j, k, l]
+n = "Chicago Sky"
+m.append(n)
+o = (i, j, k, l)
+print(c, d, e, f, i)
+print(m)
+print(o)
+
